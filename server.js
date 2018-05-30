83fc1dbedf7b787c66735ee7b858adf747757fe7
RAHERIMIHAJA Tokiniaina Nirilanto Venerand <rtnirilantov@gmail.com>
2018-05-30 17:28:22 +0300

    init app

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const moment = require('moment')
+const { Client, Pool } = require('pg')
+const { Subject } = require('rxjs')
+
+const pool = new Pool()
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json("ok ok ok!!")
+  
+  })
+  
+
+const PORT = process.NODE_PORT || 6009
+app.listen(PORT, () => console.log(`server started ... @${PORT}`))
+
