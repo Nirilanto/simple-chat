86b3f040aad733b4dde790dee5bc37a91110bf32
RAHERIMIHAJA Tokiniaina Nirilanto Venerand <rtnirilantov@gmail.com>
2018-05-31 09:58:23 +0300

    add all fuction

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const moment = require('moment')
-const { Client, Pool } = require('pg')
-const { Subject } = require('rxjs')
+const {
+  Client,
+  Pool
+} = require('pg')
+const {
+  Subject
+} = require('rxjs')
 
 const pool = new Pool()
 
@@ -13,28 +17,42 @@ const app = express()
 app.use(morgan('dev'))
 app.use(cors())
 
-app.get('/hdb_test', (req, res) => {
-    res.json("server hdb chat run!")
-  
-  })
+app.get('/hdb_test/:usend/:urecu/:msg', async (req, res) => {
+  const usend = req.params.usend
+  const urecu = req.params.urecu
+  const msg = req.params.msg
+  const all_msg = await pool.query(`INSERT INTO public.table_msg(
+   user_send, use_recue, core_msg, date_time)
+    VALUES (${usend} , ${urecu},'${msg}', now());`)
+  res.json("insert!!")
+})
+// 
+app.get('/hdb_msg', async (req, res) => {
+  const pool = new Pool()
+  const all_msg = await pool.query(`SELECT id_msg, user_send, use_recue, core_msg, date_time	
+  FROM public.table_msg`)
+  res.json(all_msg.rows)
+})
+//SELECT id_user, login_user, name_user	FROM public.table_usr;
+app.get('/hdb_user', async (req, res) => {
+  const pool = new Pool()
+  const all_user = await pool.query(`SELECT id_user, login_user, name_user
+	FROM public.table_usr;`)
+  res.json(all_user.rows)
 
-app.get('/hdb_msg', (req, res) => {
-    res.json("hdb all msg")
-  
-  })
+})
 
-app.get('/hdb_user', (req, res) => {
-    res.json("hdb all msg")
-  
-  })
-
-app.get('/hdb_add/:name/:pw', (req, res) => {
+app.get('/hdb_add/:name/:pw', async (req, res) => {
   const name = req.params.name
   const pw = req.params.pw
-  res.json([name,pw])
+  //SELECT max (id_user) as nbr	FROM public.table_usr ;
+  const nbr = await pool.query(`SELECT max (id_user) as nbr	FROM public.table_usr `)
+  let id = nbr.rows[0].nbr + 1
+  const all_user = await pool.query(`INSERT INTO public.table_usr(
+    id_user, login_user, name_user)
+    VALUES (${ id },'${ name }','${ pw }');`)
+  res.json(id)
 })
-  
 
 const PORT = process.NODE_PORT || 6031
 app.listen(PORT, () => console.log(`server started ... @${PORT}`))
-
